63b398f0e39623bb9127fdf73f8c03165373f3ca
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2021-11-24 14:52:56 -0500

    updated fix

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const booksController = require('./controllers/items');
+const itemsController = require('./controllers/items');
 // initialize app
 const app = express();
-const Book = require('./models/items')
+//const Item = require('./models/items')
 //const books = require('./controllers/books');
 // configure settings
 require('dotenv').config();
 
 // connect to and configure mongoDB with mongoose
 const DATABASE_URL = 'MONGODB_URI=mongodb+srv://admin:abc1234@cluster0.vn7x6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 mongoose.connect(DATABASE_URL);
 
 const db = mongoose.connection;
@@ -32,71 +33,68 @@ app.use('/items', itemsController);
 
 
 // Create route
-app.post('/items', (req, res) => {
-  Item.create(req.body, (err, createdItem) => {
-      res.send(createdItem);
-  }); // this code runs asynchronous
-});
+// app.post('/items', (req, res) => {
+  // Item.create(req.body, (err, createdItem) => {
+      // res.send(createdItem);
+  // }); // this code runs asynchronous
+// });
 
-app.get('/', (req, res) => {
-res.send(`Welcome to the Thrift Shop app! Go to <a href="/books">localhost:3000/items</a> to get started!`
-)
-})
+// app.get('/', (req, res) => {
+// res.send(`Welcome to the Thrift Shop app! Go to <a href="/books">localhost:3000/items</a> to get started!`
+// )
+// })
+// 
 
-
-app.get('/items', (req, res) => {
-  Book.find({}, (err, arrayOfItems) => {
-      res.send(arrayOfItems)
-      
-  });
-  res.render('index.ejs')
-});
+// app.get('/items', (req, res) => {
+  // Book.find({}, (err, arrayOfItems) => {
+      // res.send(arrayOfItems)
+      // 
+  // });
+  // res.render('index.ejs')
+// });
 
 
 // Show route
 
-app.get('/items/:id', (req, res) => {
-  Item.findById(req.params.id, (err, foundItem) => {
-      res.send('index.ejs', foundItem)
-  });
-});
-
-
-// Delete route
-app.delete('/items/:id', (req, res) => {
-  Item.findByIdAndDelete(req.params.id, (err, copyOfDeletedItem) => {
-      res.send(copyOfDeletedItem);
-  });
-});
-
-// Update route
-
-app.put('/items/:id', (req, res) => {
-  Book.findByIdAndUpdate(
-      req.params.id, 
-      req.body, 
-      { new: true }, 
-      (err, updatedItem) => {
-          res.send(updatedItem);
-  });
-});
-
-
-
-
+// app.get('/items/:id', (req, res) => {
+  // Item.findById(req.params.id, (err, foundItem) => {
+      // res.send('index.ejs', foundItem)
+  // });
+// });
+// 
+// 
+// app.delete('/items/:id', (req, res) => {
+  // Item.findByIdAndDelete(req.params.id, (err, copyOfDeletedItem) => {
+      // res.send(copyOfDeletedItem);
+  // });
+// });
+// 
+// 
+// app.put('/items/:id', (req, res) => {
+  // Book.findByIdAndUpdate(
+      // req.params.id, 
+      // req.body, 
+      // { new: true }, 
+      // (err, updatedItem) => {
+          // res.send(updated);
+  // });
+// });
+// 
+// 
+// 
+// 
 
 // Database Connection
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-})
+// mongoose.connect(DATABASE_URL, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true,
+// })
 
 // Database Connection Error/Success - optional but can be really helpful
-db.on("error", (err) => console.log(err.message + " is Mongod not running?"))
-db.on("connected", () => console.log("mongo connected"))
-db.on("disconnected", () => console.log("mongo disconnected"))
+
+
 
 
 
